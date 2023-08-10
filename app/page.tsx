bce7bab5ab18d735dac85d5e367aba151b3718c0
Ali Toshpulatov <ali.toshpulatov@edetek.com>
2023-08-10 09:38:18 +0500

    mobile version fixe

--- app/page.tsx
+++ app/page.tsx
@@ -26,7 +26,7 @@ export default function Home() {
               <a href="#" className="-m-1.5 p-1.5">
                 <span className="sr-only">MakkaExpress</span>
                 <img
-                  alt="Your Company"
+                  alt="MakkaExpress"
                   className="h-8 w-auto"
                   src="/images/logo.png"
                 />
@@ -63,11 +63,11 @@ export default function Home() {
           <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
             <div className="flex items-center justify-between">
               <a href="#" className="-m-1.5 p-1.5">
-                <span className="sr-only">Your Company</span>
+                <span className="sr-only">MakkaExpress</span>
                 <img
                   className="h-8 w-auto"
-                  src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                  alt=""
+                  src="/images/logo.png"
+                  alt="makka express logo"
                 />
               </a>
               <button
