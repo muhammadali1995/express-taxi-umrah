3f898835ad325605aa164e8e8a77662e10f31736
Zohidjon-Toshpulatov <44026027+Zohidjon-Toshpulatov@users.noreply.github.com>
2023-08-12 09:23:57 +0500

    Home page on progress

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,114 @@
+import { useState } from 'react'
+import { Dialog } from "@headlessui/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import Image from 'next/image'
+import Link from 'next/link';
+import logo from '/public/images/logo.png'
+
+const navigation = [
+    { name: "Home", href: "/" },
+    { name: "Pricing", href: "/pricing" },
+    { name: "About", href: "/about" },
+    { name: "Contact", href: "/contact" },
+    { name: "Review", href: "/review" },
+]
+
+export const Navbar = () => {
+    const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+    return (
+        <>
+            <div className="mx-auto max-w-7xl">
+                <div className="px-6 pt-6 lg:max-w-2xl lg:pl-8 lg:pr-0">
+                    <nav
+                        className="flex items-center justify-between lg:justify-start"
+                        aria-label="Global"
+                    >
+                        <Link href="/" className="-m-1.5 p-1.5">
+                            <span className="sr-only">MakkaExpress</span>
+                            <Image
+                                alt="MakkaExpress"
+                                width={100}
+                                height={100}
+                                className="h-8 max-w-[200px] w-full"
+                                src={logo}
+                            />
+                        </Link>
+                        <button
+                            type="button"
+                            className="-m-2.5 rounded-md p-2.5 text-gray-700 lg:hidden"
+                            onClick={() => setMobileMenuOpen(true)}
+                        >
+                            <span className="sr-only">Open main menu</span>
+                            <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+                        </button>
+                        <div className="hidden lg:ml-12 lg:flex lg:gap-x-14">
+                            {navigation.map((item) => (
+                                <Link
+                                    key={item.name}
+                                    href={item.href}
+                                    className="text-sm  cursor-pointer font-semibold leading-6 text-gray-900 hover:text-yellow-500"
+                                >
+                                    {item.name}
+                                </Link>
+                            ))}
+                        </div>
+                    </nav>
+                </div>
+            </div>
+            <Dialog
+                as="div"
+                className="lg:hidden"
+                open={mobileMenuOpen}
+                onClose={setMobileMenuOpen}
+            >
+                <div className="fixed inset-0 z-50" />
+                <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+                    <div className="flex items-center justify-between">
+                        <a href="#" className="-m-1.5 p-1.5">
+                            <span className="sr-only">MakkaExpress</span>
+                            <Image
+                                className="h-8 w-auto"
+                                width={100}
+                                height={32}
+                                src={logo}
+                                alt="makka express logo"
+                            />
+                        </a>
+                        <button
+                            type="button"
+                            className="-m-2.5 rounded-md p-2.5 text-gray-700"
+                            onClick={() => setMobileMenuOpen(false)}
+                        >
+                            <span className="sr-only">Close menu</span>
+                            <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+                        </button>
+                    </div>
+                    <div className="mt-6 flow-root">
+                        <div className="-my-6 divide-y divide-gray-500/10">
+                            <div className="space-y-2 py-6">
+                                {navigation.map((item) => (
+                                    <Link
+                                        key={item.name}
+                                        href={item.href}
+                                        className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                                    >
+                                        {item.name}
+                                    </Link>
+                                ))}
+                            </div>
+                            <div className="py-6">
+                                <Link
+                                    href="#"
+                                    className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                                >
+                                    Log in
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                </Dialog.Panel>
+            </Dialog>
+        </>
+
+    )
+}
---
+++ app/data/servicePrice.js
@@ -0,0 +1,574 @@
+const ServicePriceForCar = [
+    {
+        name: 'Tayota Camry',
+        model: 'CAMRY CAR Service',
+        directions: [
+            {
+                from: 'Jeddah Airport',
+                to: 'Makkah Hotel',
+                price: 300
+            }, {
+                from: 'Jeddah Airport',
+                to: 'Jeddah Hotel',
+                price: 200
+            }, {
+                from: 'Makkah Hotel',
+                to: 'Madina Hotel',
+                price: 550
+            }, {
+                from: 'Jeddah Airport',
+                to: 'Madina Hotel',
+                price: 550
+            }, {
+                from: 'Madina Airport',
+                to: 'Madina Hotel',
+                price: 200
+            }, {
+                from: 'Madina Hotel',
+                to: 'Madina Airport',
+                price: 150
+            }, {
+                from: 'Madina Ziyarat',
+                to: '',
+                price: 200
+            }, {
+                from: 'Makkah Ziyarat',
+                to: '',
+                price: 225
+            }, {
+                from: 'Jeddah',
+                to: 'Taif and Return',
+                price: 700
+            }, {
+                from: 'Makkah',
+                to: 'Taif and Return',
+                price: 500
+            }, {
+                from: 'Per Hour Rates',
+                to: '',
+                price: 100
+            }, {
+                from: 'Madina Hotel',
+                to: 'Makkah Hotel',
+                price: 550
+            }, {
+                from: 'Makkah Hotel',
+                to: 'Jeddah Airport',
+                price: 250
+            }, {
+                from: 'Masjid Ayesha Meeqat',
+                to: '',
+                price: 100
+            }, {
+                from: 'Masjid Jurana Meeqat',
+                to: '',
+                price: 150
+            }, {
+                from: 'Madina Ziarat and Wadiya Jin',
+                to: '',
+                price: 300
+            }, {
+                from: 'Makkah Hotel',
+                to: 'Train Station',
+                price: 150
+            }, {
+                from: 'Makkah Train Station',
+                to: 'Makkah Hotel',
+                price: 150
+            }, {
+                from: 'Madina Train Station',
+                to: 'Madina Hotel',
+                price: 150
+            },
+            {
+                from: 'Madina Hotel',
+                to: 'Madina Train Station',
+                price: 150
+            },
+        ],
+        imgSrc: '/images/Camry-p.png',
+        numOfSeats: 4,
+        ac: true,
+        desc: 'Umrah taxi service provide the best Car Service for Umrah in Saudi Arabia with the best and professional licensed drivers and at a very low prices'
+    },
+    {
+        name: 'HIACE',
+        model: 'HIACE - 2022 Model',
+        directions: [
+            {
+                from: 'Jeddah Airport',
+                to: 'Makkah Hotel',
+                price: 450
+            }, {
+                from: 'Jeddah Airport',
+                to: 'Jeddah Hotel',
+                price: 300
+            }, {
+                from: 'Makkah Hotel',
+                to: 'Madina Hotel',
+                price: 700
+            }, {
+                from: 'Jeddah Airport',
+                to: 'Madina Hotel',
+                price: 800
+            }, {
+                from: 'Madina Airport',
+                to: 'Madina Hotel',
+                price: 350
+            }, {
+                from: 'Madina Hotel',
+                to: 'Madina Airport',
+                price: 300
+            }, {
+                from: 'Madina Ziyarat',
+                to: '',
+                price: 400
+            }, {
+                from: 'Makkah Ziyarat',
+                to: '',
+                price: 350
+            }, {
+                from: 'Jeddah',
+                to: 'Taif and Return',
+                price: 1500
+            }, {
+                from: 'Makkah',
+                to: 'Taif and Return',
+                price: 800
+            }, {
+                from: 'Per Hour Rates',
+                to: '',
+                price: 200
+            }, {
+                from: 'Madina Hotel',
+                to: 'Makkah Hotel',
+                price: 700
+            }, {
+                from: 'Makkah Hotel',
+                to: 'Jeddah Airport',
+                price: 400
+            }, {
+                from: 'Masjid Ayesha Meeqat',
+                to: '',
+                price: 250
+            }, {
+                from: 'Masjid Jurana Meeqat',
+                to: '',
+                price: 250
+            }, {
+                from: 'Madina Ziarat and Wadiya Jin',
+                to: '',
+                price: 500
+            }, {
+                from: 'Makkah Hotel',
+                to: 'Train Station',
+                price: 250
+            }, {
+                from: 'Makkah Train Station',
+                to: 'Makkah Hotel',
+                price: 250
+            }, {
+                from: 'Madina Train Station',
+                to: 'Madina Hotel',
+                price: 250
+            },
+            {
+                from: 'Madina Hotel',
+                to: 'Madina Train Station',
+                price: 250
+            },
+        ],
+        imgSrc: '/images/Camry-p.png',
+        numOfSeats: 11,
+        ac: true,
+        desc: 'For Hajj and Umrah, we provide a pleasant and safe HiAce service.'
+    },
+    {
+        name: 'GMC 2022',
+        model: 'GMC Yukon XL / Subarban - 2022 Model',
+        directions: [
+            {
+                from: 'Jeddah Airport',
+                to: 'Makkah Hotel',
+                price: 600
+            }, {
+                from: 'Jeddah Airport',
+                to: 'Jeddah Hotel',
+                price: 400
+            }, {
+                from: 'Jeddah Hotel',
+                to: 'Jeddah Airport',
+                price: 300
+            }, {
+                from: 'Makkah Hotel',
+                to: 'Madina Hotel',
+                price: 1200
+            }, {
+                from: 'Jeddah Airport',
+                to: 'Madina Hotel',
+                price: 1200
+            }, {
+                from: 'Madina Airport',
+                to: 'Madina Hotel',
+                price: 400
+            }, {
+                from: 'Madina Hotel',
+                to: 'Madina Airport',
+                price: 300
+            }, {
+                from: 'Madina Ziyarat',
+                to: '',
+                price: 500
+            }, {
+                from: 'Makkah Ziyarat',
+                to: '',
+                price: 400
+            }, {
+                from: 'Jeddah',
+                to: 'Taif and Return',
+                price: 1500
+            }, {
+                from: 'Makkah',
+                to: 'Taif and Return',
+                price: 1000
+            }, {
+                from: 'Per Hour Rates',
+                to: '',
+                price: 200
+            }, {
+                from: 'Madina Hotel',
+                to: 'Makkah Hotel',
+                price: 1200
+            }, {
+                from: 'Makkah Hotel',
+                to: 'Jeddah Airport',
+                price: 500
+            }, {
+                from: 'Masjid Ayesha Meeqat',
+                to: '',
+                price: 300
+            }, {
+                from: 'Masjid Jurana Meeqat',
+                to: '',
+                price: 300
+            }, {
+                from: 'Madina Ziarat and Wadiya Jin',
+                to: '',
+                price: 600
+            }, {
+                from: 'Makkah Hotel',
+                to: 'Train Station',
+                price: 300
+            }, {
+                from: 'Makkah Train Station',
+                to: 'Makkah Hotel',
+                price: 300
+            }, {
+                from: 'Madina Train Station',
+                to: 'Madina Hotel',
+                price: 300
+            },
+            {
+                from: 'Madina Hotel',
+                to: 'Madina Train Station',
+                price: 300
+            },
+        ],
+        imgSrc: '/images/Camry-p.png',
+        numOfSeats: 6,
+        ac: true,
+        desc: 'Umrah taxi service provide you a lead class GMC latest model 2022 taxi service.'
+    },
+    {
+        name: 'GMC',
+        model: 'GMC Yukon XL 2016 To 2019 Model',
+        directions: [
+            {
+                from: 'Jeddah Airport',
+                to: 'Makkah Hotel',
+                price: 400
+            }, {
+                from: 'Jeddah Airport',
+                to: 'Jeddah Hotel',
+                price: 300
+            }, {
+                from: 'Jeddah Hotel',
+                to: 'Jeddah Airport',
+                price: 250
+            }, {
+                from: 'Makkah Hotel',
+                to: 'Madina Hotel',
+                price: 800
+            }, {
+                from: 'Jeddah Airport',
+                to: 'Madina Hotel',
+                price: 1000
+            }, {
+                from: 'Madina Airport',
+                to: 'Madina Hotel',
+                price: 300
+            }, {
+                from: 'Madina Hotel',
+                to: 'Madina Airport',
+                price: 250
+            }, {
+                from: 'Makkah Ziyarat',
+                to: '',
+                price: 400
+            }, {
+                from: 'Madina Ziyarat',
+                to: '',
+                price: 350
+            }, {
+                from: 'Jeddah',
+                to: 'Taif and Return',
+                price: 1500
+            }, {
+                from: 'Makkah',
+                to: 'Taif and Return',
+                price: 800
+            }, {
+                from: 'Per Hour Rates',
+                to: '',
+                price: 200
+            }, {
+                from: 'Madina Hotel',
+                to: 'Makkah Hotel',
+                price: 800
+            }, {
+                from: 'Makkah Hotel',
+                to: 'Jeddah Airport',
+                price: 400
+            }, {
+                from: 'Masjid Ayesha Meeqat',
+                to: '',
+                price: 200
+            }, {
+                from: 'Masjid Jurana Meeqat',
+                to: '',
+                price: 250
+            }, {
+                from: 'Madina Ziarat and Wadiya Jin',
+                to: '',
+                price: 500
+            }, {
+                from: 'Makkah Hotel',
+                to: 'Train Station',
+                price: 200
+            }, {
+                from: 'Makkah Train Station',
+                to: 'Makkah Hotel',
+                price: 200
+            }, {
+                from: 'Madina Train Station',
+                to: 'Madina Hotel',
+                price: 200
+            },
+            {
+                from: 'Madina Hotel',
+                to: 'Madina Train Station',
+                price: 200
+            },
+        ],
+        imgSrc: '/images/Camry-p.png',
+        numOfSeats: 7,
+        ac: true,
+        desc: 'Umrah Taxi Service provide VIP service for transportation from Jeddah to Mecca and Medina. We also have Best Ziyarat services in Mecca and Medina with our best and comfortable vehicles GMC .'
+    },
+    {
+        name: 'Coaster',
+        model: 'COASTER Service',
+        directions: [
+            {
+                from: 'Jeddah Airport',
+                to: 'Makkah Hotel',
+                price: 800
+            }, {
+                from: 'Jeddah Airport',
+                to: 'Jeddah Hotel',
+                price: 500
+            }, {
+                from: 'Jeddah Hotel',
+                to: 'Jeddah Airport',
+                price: 400
+            }, {
+                from: 'Makkah Hotel',
+                to: 'Madina Hotel',
+                price: 1200
+            }, {
+                from: 'Jeddah Airport',
+                to: 'Madina Hotel',
+                price: 1200
+            }, {
+                from: 'Madina Airport',
+                to: 'Madina Hotel',
+                price: 500
+            }, {
+                from: 'Madina Hotel',
+                to: 'Madina Airport',
+                price: 500
+            }, {
+                from: 'Madina Ziyarat',
+                to: '',
+                price: 550
+            }, {
+                from: 'Makkah Ziyarat',
+                to: '',
+                price: 600
+            }, {
+                from: 'Jeddah',
+                to: 'Taif and Return',
+                price: 1800
+            }, {
+                from: 'Makkah',
+                to: 'Taif and Return',
+                price: 1200
+            }, {
+                from: 'Per Hour Rates',
+                to: '',
+                price: 300
+            }, {
+                from: 'Madina Hotel',
+                to: 'Makkah Hotel',
+                price: 1200
+            }, {
+                from: 'Makkah Hotel',
+                to: 'Jeddah Airport',
+                price: 700
+            }, {
+                from: 'Masjid Ayesha Meeqat',
+                to: '',
+                price: 300
+            }, {
+                from: 'Masjid Jurana Meeqat',
+                to: '',
+                price: 400
+            }, {
+                from: 'Madina Ziarat and Wadiya Jin',
+                to: '',
+                price: 650
+            }, {
+                from: 'Makkah Hotel',
+                to: 'Train Station',
+                price: 400
+            }, {
+                from: 'Makkah Train Station',
+                to: 'Makkah Hotel',
+                price: 400
+            }, {
+                from: 'Madina Train Station',
+                to: 'Madina Hotel',
+                price: 400
+            },
+            {
+                from: 'Madina Hotel',
+                to: 'Madina Train Station',
+                price: 400
+            },
+        ],
+        imgSrc: '/images/Camry-p.png',
+        numOfSeats: 17,
+        ac: true,
+        desc: 'Umrah Taxi Service provide the best coaster service for Hajj and Umrah pilgrims in all over the Saudi Arabia.'
+    },
+    {
+        name: 'H1',
+        model: 'H1 HYUNDAI Service',
+        directions: [
+            {
+                from: 'Jeddah Airport',
+                to: 'Makkah Hotel',
+                price: 350
+            }, {
+                from: 'Jeddah Airport',
+                to: 'Jeddah Hotel',
+                price: 250
+            }, {
+                from: 'Jeddah Hotel',
+                to: 'Jeddah Airport',
+                price: 200
+            }, {
+                from: 'Makkah Hotel',
+                to: 'Madina Hotel',
+                price: 600
+            }, {
+                from: 'Jeddah Airport',
+                to: 'Madina Hotel',
+                price: 600
+            }, {
+                from: 'Madina Airport',
+                to: 'Madina Hotel',
+                price: 200
+            }, {
+                from: 'Madina Hotel',
+                to: 'Madina Airport',
+                price: 150
+            }, {
+                from: 'Madina Ziyarat',
+                to: '',
+                price: 250
+            }, {
+                from: 'Makkah Ziyarat',
+                to: '',
+                price: 300
+            }, {
+                from: 'Jeddah',
+                to: 'Taif and Return',
+                price: 800
+            }, {
+                from: 'Makkah',
+                to: 'Taif and Return',
+                price: 600
+            }, {
+                from: 'Per Hour Rates',
+                to: '',
+                price: 150
+            }, {
+                from: 'Madina Hotel',
+                to: 'Makkah Hotel',
+                price: 600
+            }, {
+                from: 'Makkah Hotel',
+                to: 'Jeddah Airport',
+                price: 300
+            }, {
+                from: 'Masjid Ayesha Meeqat',
+                to: '',
+                price: 150
+            }, {
+                from: 'Masjid Jurana Meeqat',
+                to: '',
+                price: 4150
+            }, {
+                from: 'Madina Ziarat and Wadiya Jin',
+                to: '',
+                price: 400
+            }, {
+                from: 'Makkah Hotel',
+                to: 'Train Station',
+                price: 150
+            }, {
+                from: 'Makkah Train Station',
+                to: 'Makkah Hotel',
+                price: 150
+            }, {
+                from: 'Madina Train Station',
+                to: 'Madina Hotel',
+                price: 150
+            },
+            {
+                from: 'Madina Hotel',
+                to: 'Madina Train Station',
+                price: 150
+            },
+        ],
+        imgSrc: '/images/H1.png',
+        numOfSeats: 7,
+        ac: true,
+        desc: 'Umrah Taxi Service provide excellent Hyundai service for Hajj and Umrah pilgrims with very low and reasonable prices.'
+    }
+
+];
+
+export default ServicePriceForCar
+
